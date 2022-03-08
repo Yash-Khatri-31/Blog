66d813c6dd7a2776c4f27296c44cf347c199b80b
Yash-Khatri-31 <yashr124@gmail.com>
2022-03-09 02:53:58 +0400

    Added Moderation with changes

--- eventbus/index.js
+++ eventbus/index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const cors = require('cors');
 
 const app = express();
-app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/events', (req,res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event).catch(err => console.log(err))
-    axios.post('http://localhost:5000/events', event).catch(err => console.log(err))
-    axios.post('http://localhost:4002/events', event).catch(err => console.log(err)) // Query Service
+    axios.post('http://localhost:4000/events', event).catch((err) => console.log(err.message))
+    axios.post('http://localhost:5000/events', event).catch((err) => console.log(err.message))
+    axios.post('http://localhost:4003/events', event).catch((err) => console.log(err.message))
+    axios.post('http://localhost:4002/events', event).catch((err) => console.log(err.message)) // Query Service
 
     res.send({status:'OK'})
 });
