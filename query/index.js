b97023a32b12903ecdd73ef86ea6e3466513f256
Yash-Khatri-31 <yashr124@gmail.com>
2022-03-09 10:34:23 +0400

    Added endpoint to get all events

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios')
 
 const app = express();
 
@@ -10,14 +11,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req,res) => {
-    res.send(posts)
-});
-
-app.post('/events', (req,res) => {
-
-    const {type, data} = req.body;
-
+const handleEvents = (type,data) => {
     if(type === 'PostCreated'){
 
         const {id, title} = data;
@@ -39,13 +33,33 @@ app.post('/events', (req,res) => {
         comment.status = status;
         comment.content = content;
     }
+}
 
-    console.log(posts)
+app.get('/posts', (req,res) => {
+    res.send(posts)
+});
+
+app.post('/events', (req,res) => {
+
+    const {type, data} = req.body;
+
+    handleEvents(type,data);
 
     res.send({});
 })
 
-app.listen(4002, () => {
-    console.log('Listening on Port 4002')
+app.listen(4002, async () => {
+    console.log('Listening on Port 4002');
+
+    try {
+        const res = await axios.get('http://localhost:4005/events');
+
+        for(let event of res.data){
+            console.log('Processing event', event.type)
+            handleEvents(event.type, event.data);
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
 })
 
